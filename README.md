84a0c0dba481763a4773541a1ec5455548f4c9a2
perfect perfect <gilberto.j.covarrubias@gmail.com>
2022-01-17 00:06:20 -0500

    tidying up spacing and finishing README

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
-# coding-quiz
+# Coding Quiz
+![Screenshot](./assets/images/coding-quiz-screenshot.png)
+
+## Description
+---
+A coding quiz that increases its replayability by using and maintaining a high score component. The quiz is both fun and helps reinforce coding principles. You first arrive to the page and prmpted to begin the quiz. Once the quiz begins a timer starts. The timer also simultaeously serves as the scoring component. The faster you complete the quiz the higher your score. 
+
+## Features
+---
+Uses Jquery and JavaScript to dynamically create HTML. Also utilizes local storage to create a maintain the high score. Also, uses a timer to create a time-limit and to be used as the scoring component.
+
+## Usage
+---
+To use, go to the website, then cick the "Go!" button. Then questions will propogate one by one. The goal is to complete the quiz as fast as possible. If you beat the high score you sill be asked for your initials.
+
+## link to website/environment
+---
+[Coding-Quiz](https://perfect-perfect.github.io/coding-quiz/)
+
+## License
+---
+MIT License
+
+Copyright (c) 2021 Gilberto Jesus Covarrubias Merino
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,7 @@ var playerScore = 100;
 var timerP = document.getElementById("timer");
 var timerInterval;
 var questions = [
+
     {question: "What does HTML stand for?",
     choices: ["Honda Toyota Minor League","How To Make Language","Hyper Text Markup Language"],
     answer: "Hyper Text Markup Language"},
@@ -20,6 +21,7 @@ var questions = [
 
 // timer/score
 var timer = function() {
+
     playerScore--;
     timerP.textContent=playerScore;
 
@@ -33,14 +35,12 @@ var timer = function() {
 
 // creates intro screen to game
 var preGame = function() {
+
     // creat containers
     var  quizContainerEl = document.createElement("div")
     quizContainerEl.className = "quiz-container"
-
     var questionContainerEl = document.createElement("div");
-
     questionContainerEl.innerHTML = "<h1> Begin Quiz ?<h3>"
-
     quizContainerEl.appendChild(questionContainerEl);
 
     // create button
@@ -59,7 +59,6 @@ var promptUser = function(event) {
 
     var targetEl = event.target;
 
-
     if (targetEl.matches(".go")) {
         quizContentEl.innerHTML = "";
         timerInterval = setInterval(timer,'1000');
@@ -73,12 +72,12 @@ var quiz = function() {
 
     // this gets the object from the array
     var currentQuestion = questions[questionCounter];
-
     var questionEl = document.createElement("div");
     questionEl.textContent = currentQuestion.question;
     quizContentEl.appendChild(questionEl);
 
     currentQuestion.choices.forEach(function(choice){
+
         var optionEl = document.createElement("button");
         optionEl.textContent = choice;
         optionEl.setAttribute("data-answer", choice)
@@ -87,7 +86,6 @@ var quiz = function() {
 
     })
 
-    
 };
 
 // initiates whether we get a question or end the game
@@ -95,15 +93,17 @@ var getQuestion = function(){
     
     quizContentEl.innerHTML = "";
 
-
     if (questionCounter >= 3) {
+
         endGame();
+
     }
     else {
+
         quiz();
+
     }
 
-    
 };
 
 // checks if answer is correct
@@ -112,6 +112,7 @@ var checkAnswer = function() {
     var answerClick = this.getAttribute("data-answer")
 
     if (answerClick!==questions[questionCounter].answer) {
+
         playerScore = playerScore - 20;
         alert("Wrong! You have been deducted 20 seconds")
 
@@ -126,32 +127,34 @@ var checkAnswer = function() {
 var endGame = function() {
     
     clearInterval(timerInterval);
-
     timerP.textContent = "";
 
     quizContentEl.textContent = "";
-
+    
     alert("The game has ended")
-
     var highScore = localStorage.getItem("highscore");
 
     if (highScore === null) {
-        highScore = 0;
-    }
 
+        highScore = 0;
+
+    }
 
     // if the player has more money than the highscore, the player has a new high score!
     if(playerScore > highScore) {
-        var playerName = prompt("You have beaten the High Score! Enter intials to save High Score!");
 
+        var playerName = prompt("You have beaten the High Score! Enter intials to save High Score!");
         localStorage.setItem("highscore", playerScore);
         localStorage.setItem("name", playerName);
-
         alert(playerName + " now has the High Score of " + playerScore);
+
     }
     else {
+
         alert("Your score of " + playerScore + " did not beat the high score of " + highScore);
+
     }
+
 };
 
 // lauches entry screen
